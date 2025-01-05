0f3e1fd574976f2e3615fdb3bb9468ae93f394db
Houria Khanjar <gls97048@students.codeinstitute.net>
2025-01-05 15:10:48 +0000

    add event listener to login buttons, and check if any teacher is logged in

--- assets/js/helper.js
+++ assets/js/helper.js
@@ -1,5 +1,54 @@
-// This file contains helper functions avoiding duplicate code
+// This file contains helper functions to avoid duplicate code
 
+/**
+ * Redirect to pageName
+ */
+function redirect(pageName){
+    const newLocation = window.location.href.replace(/\/[^\/]*$/, `/${pageName}`);
+    window.location.replace(newLocation);
+}
+
+
+
+/**
+ * check if any teacher was logged in
+ */
+function loginCheck(key) {
+    return getCookie(key)
+}
+
+/**
+ * save loggedIn teacher id in browser cookies
+ */
+function loggedIn(key, teacherId) {
+    return setCookie(key, teacherId)
+}
+
+/**
+ * Get cookie by its name
+ */
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return null;
+}
+
+/**
+ * Set cookie
+ */
+function setCookie(cname, cvalue) {
+    document.cookie = cname + "=" + cvalue + ";path=/";
+}
 
 /**
  * Feting Data from JSON File
@@ -23,3 +72,4 @@ function hideLoader() {
     let loader = document.getElementById("loader");
     loader.classList.add("hide");
 }
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,48 @@
+/**
+ * 
+ * 
+ * This file will control the start page (index.html) 
+ * 1. Ensure that no teacher is logged in by checking the value 
+ *    of the variable (loggedin_teacher_id) in the cookies.
+ *    If the loggedin_teacher_id is not null, it will redirect to the teacher’s account. 
+ * 
+ * 
+ * 2. After the documet conetnt loaded 
+ *    the teachers will be fetched from teaches.json file 
+ *    and the teachers' data will be displayed in the teachers table on the start page.
+ * 
+ * 3. add event listener to login button, 
+ *    when a login button clicked it will take
+ *    the user to teacher account page
+ *    and update login data(loggedin_teacher_id) to cookies
+ * 
+ * 
+ * 
+ */
+
+
+
+/**
+ * call function (loginCheck), which was declared in helper file 
+ * before fetching the teachers data.
+ * If the loggedin_teacher_id is not null, it will redirect to the teacher’s account. 
+ */
+window.onload = function () {
+    //call loginCheck function wil loggedin_teacher_id key to chaeck if this key appers in cookies
+    const isLoggedin = loginCheck("loggedin_teacher_id");
+    if(isLoggedin) {
+        redirect('account.html')
+    }
+
+}
+
+
+
+/**
+ * After the documet conetnt loaded  fetch teachers and display them.
+ * After finishing loading teachers data, the event listener on login buttons will be added
+ */
+
 document.addEventListener("DOMContentLoaded", function () {
 
     // fetch teacher data from teachers.json files then fill teachers table on start page by calling fetchTeachers function
@@ -26,7 +71,7 @@ document.addEventListener("DOMContentLoaded", function () {
     function fillTeachersTable(data) {
         if (data) {
             let teachersTable = document.querySelector("#teachers-table tbody");
-            for (teacher of data) {
+            for (let teacher of data) {
                 let newRow = document.createElement("tr");
                 newRow.innerHTML = ` <th scope="row">${teacher.username}</th>
                                     <td>
@@ -38,8 +83,10 @@ document.addEventListener("DOMContentLoaded", function () {
                                     </td>`;
                 teachersTable.appendChild(newRow);
             }
-            // call hide loader function, which was declared in helper.js file to hide the loader after fetching data 
+            //call hide loader function, which was declared in helper.js file to hide the loader after fetching data 
            hideLoader();
+           //after loading teachers on teachers table add event listener to login button
+           addEventListenerLoginButton();
         } else {
             displayTeachersNotFound();
         }
@@ -57,6 +104,24 @@ document.addEventListener("DOMContentLoaded", function () {
         teachersTable.appendChild(newRow);
         // call hide loader function, which was declared in helper.js file to hide the loader after fetching data 
         hideLoader();
+    }
+
+    /**
+     * add event listener to login button
+     */
+    function addEventListenerLoginButton() {
+        let loginButtons = document.querySelectorAll("#teachers-table .login-button");
+        for (let button of loginButtons){
+              button.addEventListener("click", function(e){
+                //get teacher id from attribute data-teacher-id
+                const teacherId= e.target.getAttribute("data-teacher-id");
+                //save logged in teacher id in browser cookies
+                loggedIn("loggedin_teacher_id", teacherId);
+                //redirect to teacher account
+                redirect('account.html')
+              });
+        }
+
     }
 
 });
